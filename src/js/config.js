1d01f6649ff84b05836eaf7015c99de6aa469518
Phaham <phahamkhan17@gmail.com>
2022-05-29 04:15:09 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'becb07d7-87e5-4f01-a84a-d286063448ad';
+export const MODAL_CLOSER_SEC = 2.5;
